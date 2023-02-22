38338b171d23880f043360813d07e0a433ca2b82
KasumiNova <3044344887@qq.com>
2023-02-22 23:49:30 +0800

    Fix not added annotations.

--- src/main/java/hellfirepvp/modularmachinery/common/integration/crafttweaker/event/machine/MachineTickEvent.java
+++ src/main/java/hellfirepvp/modularmachinery/common/integration/crafttweaker/event/machine/MachineTickEvent.java
@@ -1,7 +1,11 @@
 package hellfirepvp.modularmachinery.common.integration.crafttweaker.event.machine;
 
+import crafttweaker.annotations.ZenRegister;
 import hellfirepvp.modularmachinery.common.integration.crafttweaker.event.IMachineController;
+import stanhebben.zenscript.annotations.ZenClass;
 
+@ZenRegister
+@ZenClass("mods.modularmachinery.MachineTickEvent")
 public class MachineTickEvent extends MachineEvent {
     public MachineTickEvent(IMachineController controller) {
         super(controller);
