xinyihl/ModularMachinery-Community-Edition

commit 6a800d9b91dd6cda23f4fe5422be330bf847c0dd

Fix item is NOT consumed.

src/main/java/hellfirepvp/modularmachinery/common/crafting/requirement/RequirementItem.java
@@ -308,8 +308,8 @@ public class RequirementItem extends ComponentRequirement<ItemStack, Requirement
                     }
 
                     boolean can = (this.nbtChecker != null)
-                            ? ItemUtils.consumeFromInventory(handler, stackRequired, true, this.nbtChecker)
-                            : ItemUtils.consumeFromInventory(handler, stackRequired, true, this.tag);
+                            ? ItemUtils.consumeFromInventory(handler, stackRequired, false, this.nbtChecker)
+                            : ItemUtils.consumeFromInventory(handler, stackRequired, false, this.tag);
                     if (chance.canProduce(productionChance)) {
                         return can;
                     }
@@ -317,15 +317,15 @@ public class RequirementItem extends ComponentRequirement<ItemStack, Requirement
                 case OREDICT:
                     int requiredOredict = Math.round(RecipeModifier.applyModifiers(context, this, this.oreDictItemAmount, false));
                     can = (this.nbtChecker != null)
-                            ? ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, true, nbtChecker)
-                            : ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, true, this.tag);
+                            ? ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, false, nbtChecker)
+                            : ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, false, this.tag);
                     if (chance.canProduce(productionChance)) {
                         return can;
                     }
                     return can;
                 case FUEL:
                     int requiredBurnTime = Math.round(RecipeModifier.applyModifiers(context, this, this.fuelBurntime, false));
-                    can = ItemUtils.consumeFromInventoryFuel(handler, requiredBurnTime, true, this.tag) <= 0;
+                    can = ItemUtils.consumeFromInventoryFuel(handler, requiredBurnTime, false, this.tag) <= 0;
                     if (chance.canProduce(productionChance)) {
                         return can;
                     }

src/main/java/hellfirepvp/modularmachinery/common/machine/TaggedPositionBlockArray.java
@@ -24,6 +24,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * Date: 04.03.2019 / 21:33
  */
 public class TaggedPositionBlockArray extends BlockArray {
+    //TODO 预先构建已翻转的 BlockArray。
 
     private final Map<BlockPos, ComponentSelectorTag> taggedPositions = new ConcurrentHashMap<>();
 

commit 94e9dc1975ea8a25b8ded501c585d1ded8546c0d

Fix an issue caused by 6a800d9 where the probability does not take effect.

src/main/java/hellfirepvp/modularmachinery/common/crafting/requirement/RequirementItem.java
@@ -307,25 +307,27 @@ public class RequirementItem extends ComponentRequirement<ItemStack, Requirement
                         }
                     }
 
-                    boolean can = (this.nbtChecker != null)
+                    if (chance.canProduce(productionChance)) {
+                        return (this.nbtChecker != null)
+                                ? ItemUtils.consumeFromInventory(handler, stackRequired, true, this.nbtChecker)
+                                : ItemUtils.consumeFromInventory(handler, stackRequired, true, this.tag);
+                    }
+                    return (this.nbtChecker != null)
                             ? ItemUtils.consumeFromInventory(handler, stackRequired, false, this.nbtChecker)
                             : ItemUtils.consumeFromInventory(handler, stackRequired, false, this.tag);
-                    if (chance.canProduce(productionChance)) {
-                        return can;
-                    }
-                    return can;
                 case OREDICT:
                     int requiredOredict = Math.round(RecipeModifier.applyModifiers(context, this, this.oreDictItemAmount, false));
-                    can = (this.nbtChecker != null)
+                    if (chance.canProduce(productionChance)) {
+                        return (this.nbtChecker != null)
+                                ? ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, true, nbtChecker)
+                                : ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, true, this.tag);
+                    }
+                    return (this.nbtChecker != null)
                             ? ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, false, nbtChecker)
                             : ItemUtils.consumeFromInventoryOreDict(handler, this.oreDictName, requiredOredict, false, this.tag);
-                    if (chance.canProduce(productionChance)) {
-                        return can;
-                    }
-                    return can;
                 case FUEL:
                     int requiredBurnTime = Math.round(RecipeModifier.applyModifiers(context, this, this.fuelBurntime, false));
-                    can = ItemUtils.consumeFromInventoryFuel(handler, requiredBurnTime, false, this.tag) <= 0;
+                    boolean can = ItemUtils.consumeFromInventoryFuel(handler, requiredBurnTime, false, this.tag) <= 0;
                     if (chance.canProduce(productionChance)) {
                         return can;
                     }