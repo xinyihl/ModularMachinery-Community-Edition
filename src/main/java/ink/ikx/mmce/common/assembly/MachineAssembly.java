99f3dba999d5b6d010382a73ac35f49f8740471d
Hikari_Nova <3044344887@qq.com>
2023-06-07 10:57:03 +0800

    Forge Bucket and Vanilla Bucket are temporarily unavailable for use in assembly materials; Lower the version requirement of TConstruct.

--- src/main/java/ink/ikx/mmce/common/assembly/MachineAssembly.java
+++ src/main/java/ink/ikx/mmce/common/assembly/MachineAssembly.java
@@ -10,7 +10,9 @@ import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.Tuple;
@@ -21,6 +23,7 @@ import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.IFluidBlock;
+import net.minecraftforge.fluids.UniversalBucket;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
 import java.util.ArrayList;
@@ -105,6 +108,11 @@ public class MachineAssembly {
     private static List<IFluidHandlerItem> getFluidHandlerItems(final List<ItemStack> inventory) {
         List<IFluidHandlerItem> fluidHandlers = new ArrayList<>();
         for (final ItemStack invStack : inventory) {
+            Item item = invStack.getItem();
+            // TODO Bucket are not supported at this time.
+            if (item instanceof UniversalBucket || item == Items.LAVA_BUCKET || item == Items.WATER_BUCKET) {
+                continue;
+            }
             if (!FluidUtils.isFluidHandler(invStack)) {
                 continue;
             }
