dfed169150dddc41b7de41316353807a1b6937f0
Hikari_Nova <3044344887@qq.com>
2024-07-13 21:02:10 +0800

    - Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 // Project properties
 group = "hellfirepvp.modularmachinery"
-version = "2.0.0-pre7"
+version = "2.0.0-pre8"
 
 // Set the toolchain version to decouple the Java we run Gradle with from the Java used to compile and run the mod
 java {
